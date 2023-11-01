8a7f2ac87dac800a69db98da746ebb42a595d0b2
Brian Onyeneke <b.onyeneke@yahoo.com>
2023-11-01 19:32:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ provider "azurerm" {
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
+# Update this block with the location of your terraform state file
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
   location = var.location
